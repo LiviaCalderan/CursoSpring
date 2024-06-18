66df40a786b3d5d37db9b4cd87d87068c29de324
Lívia Calderan <liv_calderan@hotmail.com>
2024-06-18 19:53:48 -0400

    Estatisticas sobre episodios

--- screenMatch/src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
+++ screenMatch/src/main/java/br/com/alura/screenMatch/ScreenMatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.screenMatch;
 
-import br.com.alura.screenMatch.model.DadosEpisodio;
-import br.com.alura.screenMatch.model.DadosSerie;
-import br.com.alura.screenMatch.model.DadosTemporada;
-import br.com.alura.screenMatch.service.ConsumoApi;
-import br.com.alura.screenMatch.service.ConverteDados;
+import br.com.alura.screenMatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner{
@@ -21,27 +14,9 @@ public class ScreenMatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception { //funciona como main
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi
-				.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=546e4c2f");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=546e4c2f");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas() ; i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=546e4c2f");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-
-		temporadas.forEach(System.out::println);
 	}
 
 }
--- screenMatch/src/main/java/br/com/alura/screenMatch/principal/Principal.java
+++ screenMatch/src/main/java/br/com/alura/screenMatch/principal/Principal.java
@@ -1,22 +1,122 @@
 package br.com.alura.screenMatch.principal;
 
+import br.com.alura.screenMatch.model.DadosEpisodio;
+import br.com.alura.screenMatch.model.DadosSerie;
+import br.com.alura.screenMatch.model.DadosTemporada;
+import br.com.alura.screenMatch.model.Episodio;
 import br.com.alura.screenMatch.service.ConsumoApi;
+import br.com.alura.screenMatch.service.ConverteDados;
 
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
-    public void exibeMenu(){
 
-        private Scanner leitura = new Scanner(System.in);
-        private ConsumoApi consumo = new ConsumoApi();
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=546e4c2f";
 
-        private final String ENDERECO = "https://www.omdbapi.com/?t=";
-        private final String API_KEY = "&apikey=546e4c2f";
+    public void exibeMenu() {
 
         System.out.println("Digite o nome da serie: ");
         var nomeSerie = leitura.nextLine();
-        var json = consumoApi
+        var json = consumo
                 .obterDados(ENDERECO + nomeSerie.replaceFirst(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
 
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo
+                    .obterDados(ENDERECO + nomeSerie.replaceFirst(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodio = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+//        System.out.println("Top 10 episódios");
+//        dadosEpisodio.stream()
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("Primeiro filtro(N/A) " + e)) //permite visualizar o que está acontecendo em cada etapa da stream
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .peek(e -> System.out.println("Ordenação "))
+//                .limit(10)
+//                .peek(e -> System.out.println("Limite "))
+//                .map(e -> e.titulo().toUpperCase())
+//                .peek(e -> System.out.println("Mapeamento "))
+//                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream().map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("Digite um trecho do titulo do episódio: ");
+        var trechoTitulo = leitura.nextLine();
+
+        Optional<Episodio> episodioBuscado = episodios.stream() //usando optional para n ter referencia nula
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst(); //retorna um Optiona; Primeira opção encontrada que atende aos critérios.
+
+        if(episodioBuscado.isPresent()) {
+            System.out.println("Episódio encontrado!");
+            System.out.println("Nome: " + episodioBuscado.get().getTitulo());
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+        } else {
+            System.out.println("Episodio não encontrado!");
+        }
+
+//       System.out.println("A partir de qual ano você deseja ver os episódios?");
+//        var ano = leitura.nextInt();
+//        leitura.nextLine();
+//
+//        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+//
+//        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        episodios.stream()
+//                .filter(e -> e.getDataDeLancamento() != null && e.getDataDeLancamento().isAfter(dataBusca))
+//                .forEach(e -> System.out.println(
+//                        "Temporada:  " + e.getTemporada() +
+//                                "Episódio: " + e.getTitulo() +
+//                                "Data lançamento: " + e.getDataDeLancamento().format(formatador)
+//                ));
+
+        //media das temporadas
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getAvaliacao)));
+
+
+        System.out.println(avaliacoesPorTemporada);
+
+        //classe utilitária que permite calcular estatísticas como soma, média, valor mínimo e máximo
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+
+        System.out.println("media: " + est.getAverage());
+        System.out.println("Min: " + est.getMin());
+        System.out.println("Max: " + est.getMax());
+        System.out.println("Quantidade: " + est.getCount());
     }
 }
